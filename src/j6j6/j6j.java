ea6aac16962977acb46000fe468654a7f9c95c2f
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-01-17 18:02:07 +0300

    m

--- src/j6j6/j6j.java
+++ src/j6j6/j6j.java
@@ -1,6 +1,8 @@
 package j6j6;
 
-public class j6j {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class j6j extends JavaPlugin {
    // String хуй соси;
     //String нет, ты соси хуй гыгыгыгыыввыыв;
 }
